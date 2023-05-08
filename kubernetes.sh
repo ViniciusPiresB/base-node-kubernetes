e3d7e818a284c90992f4892b4f92bba63b2138ac
ViniPiresB <viniciuspbarreto1@gmail.com>
2023-05-08 20:38:24 -0300

    ✨ feat: kube config

--- kubernetes.sh
+++ kubernetes.sh
@@ -4,8 +4,14 @@ docker tag viniciuspiresb/base-node-kubernetes:latest viniciuspiresb/base-node-k
 
 docker push viniciuspiresb/base-node-kubernetes
 
-minibuke start
+minikube start
 
 kubectl create -f kubeconfig/postgres-sts.json
 
-kubectl get sts
+kubectl create -f kubeconfig/postgres-svc.json
+
+kubectl create -f kubeconfig/api-deployments.json
+
+kubectl create -f kubeconfig/api-svc.json
+
+minikube service api-demo-svc --url
